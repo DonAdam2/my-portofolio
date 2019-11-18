37536f7ea7386c243e170096c07d3075e25de523
Adam Morsi <adam.morsi@yahoo.com>
2019-11-18 21:29:18 +0200

    update banner text

--- src/js/components/Banner.jsx
+++ src/js/components/Banner.jsx
@@ -7,13 +7,13 @@ const Banner = () => (
 			<div className="quote">
 				<h1>Hi, my name is Adam!</h1>
 				<p>
-					I'm
+					I'm{' '}
 					<Typewriter
 						sentencesText={[
-							' a software developer.',
-							' creative.',
-							' in Love with web development.',
-							' your next web guy.',
+							'a software developer.',
+							'creative.',
+							'in Love with web development.',
+							'your next web guy.',
 						]}
 						typingSpeed={100}
 						isInfinite={true}
