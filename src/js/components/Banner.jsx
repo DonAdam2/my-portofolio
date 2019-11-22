95383d41f854cb4153e19806f4eeebb2a621cdad
a_morsi <adam.morsi@mindgeek.com>
2019-11-22 10:21:10 +0200

    update banner

--- src/js/components/Banner.jsx
+++ src/js/components/Banner.jsx
@@ -2,26 +2,40 @@ import React from 'react';
 import Typewriter from './UI/Typewriter';
 
 const Banner = () => (
-	<div className=" banner">
-		<div className="bg">
-			<div className="quote">
-				<h1>Hi, my name is Adam!</h1>
-				<p>
-					I'm{' '}
-					<Typewriter
-						sentencesText={[
-							'a software developer.',
-							'creative.',
-							'in Love with web development.',
-							'your next web guy.',
-						]}
-						typingSpeed={100}
-						isInfinite={true}
-					/>
-				</p>
-			</div>
-			<div className="image-overlay" />
+	<div className="intro">
+		<div className="quote">
+			<h1>Hi, my name is Adam!</h1>
+			<p>
+				I'm{' '}
+				<Typewriter
+					sentencesText={[
+						'a software developer.',
+						'creative.',
+						'in Love with web development.',
+						'your next web guy.',
+					]}
+					typingSpeed={100}
+					isInfinite={true}
+				/>
+			</p>
 		</div>
+		<div className="squares-wrapper">
+			<ul className="squares">
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+				<li />
+			</ul>
+		</div>
+		<div className="image-overlay" />
 	</div>
 );
 
