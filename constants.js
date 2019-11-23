6a2f07e101848aa92265be776f0bda439a9c8cda
Adam Morsi <adam.morsi@yahoo.com>
2019-11-23 19:42:28 +0200

    update about me

--- constants.js
+++ constants.js
@@ -7,7 +7,7 @@ module.exports = {
 	metaInfo: {
 		title: 'Adam Morsi Portfolio',
 		description:
-			'Hello, I am adam, I am a software developer currently based in Nicosia, Cyprus. I love creating responsive web applications. I love learning and being up to date with latest technologies',
+			'Hello, I am adam, i am a versatile Software Developer with 2+ years of experience designing, developing, and managing complex sites and internal frameworks. Specializes in Angular 2+, React and responsive design.',
 		url: 'https://adam-morsi.com/',
 		keywords: 'Adam Morsi, portfolio, software developer',
 	},
