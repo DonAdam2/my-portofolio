5425ece9c267811f70dfd99b3e5837c4d1da45d8
DonAdam2 <adam.morsi@yahoo.com>
2022-12-29 22:55:39 +0200

    drop console in production

--- src/serviceWorker/swRegistration.js
+++ src/serviceWorker/swRegistration.js
@@ -5,23 +5,15 @@ export default function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
       const wb = new Workbox('serviceWorker.js');
 
-      /*wb.addEventListener('installed', (event) => {
+      wb.addEventListener('installed', (event) => {
         if (event.isUpdate) {
           if (confirm('New app update is available, Click ok to refresh')) {
             window.location.reload();
           }
         }
-      });*/
+      });
 
-      wb.register()
-        .then(function (registration) {
-          console.log('Service Worker Registered', registration);
-        })
-        .catch(function (err) {
-          console.log('Service Worker Failed to Register', err);
-        });
+      wb.register();
     }
-  } else {
-    return;
   }
 }
