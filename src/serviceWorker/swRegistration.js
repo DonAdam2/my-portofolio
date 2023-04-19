24eb5256479167c2f06322b1548aa157a9e3ed22
DonAdam2 <adam.morsi@yahoo.com>
2023-04-19 23:30:02 +0300

    update service worker

--- src/serviceWorker/swRegistration.js
+++ src/serviceWorker/swRegistration.js
@@ -1,9 +1,20 @@
 import { Workbox } from 'workbox-window';
 
-export default async function registerServiceWorker() {
+export default function registerServiceWorker() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     const wb = new Workbox('serviceWorker.js');
 
-    await wb.register();
+    wb.unregister();
+
+    caches
+      .keys()
+      .then((cacheNames) => {
+        cacheNames.forEach((cacheName) => {
+          caches.delete(cacheName);
+        });
+      })
+      .then(() => {
+        wb.register();
+      });
   }
 }
