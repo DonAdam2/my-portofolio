15603045525cd5e5ee3fae6679ced4554cd00647
“ <adam.morsi@yahoo.com>
2024-06-20 12:13:09 +0300

    use intersection observer in lazy image component

--- src/js/components/shared/LazyImage.jsx
+++ src/js/components/shared/LazyImage.jsx
@@ -2,63 +2,37 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 //images
 import spinner from '@/public/assets/images/spinner.gif';
 
-const LazyImage = ({ src, alt, className, loaderIcon, scrollableNode }) => {
-  const loaderSrc = loaderIcon ? loaderIcon : spinner,
-    [imgSrc, setImgSrc] = useState(loaderSrc),
-    imageRef = useRef(null),
-    loader = useMemo(() => new Image(), []);
-
-  const elementInViewPort = () => {
-    // getBoundingClientRect => returns the size of the given element and the position of it in relation to the view port
-    const clientRect = imageRef.current.getBoundingClientRect();
-
-    return (
-      clientRect.top >= 0 &&
-      clientRect.left >= 0 &&
-      clientRect.bottom - 100 <= (window.innerHeight || document.documentElement.clientHeight) &&
-      clientRect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  };
-
-  const lazyLoad = useCallback(() => {
-    if (elementInViewPort()) {
-      setImgSrc(src);
-    }
-  }, [src]);
+const LazyImage = ({ src, alt, className, loaderIcon }) => {
+  const [imgSrc, setImgSrc] = useState(loaderIcon ? loaderIcon : spinner),
+    imageRef = useRef(null);
 
   useEffect(() => {
-    lazyLoad();
-  }, [lazyLoad]);
+    loadImage(imageRef.current);
+  }, []);
 
-  const checkImage = useCallback(() => {
-    if (loader.complete) {
-      lazyLoad();
-    }
-  }, [lazyLoad, loader.complete]);
-
-  useEffect(() => {
-    if (scrollableNode) {
-      //loader is used to run lazyLoad method on load (note: if we removed the loader functionality the lazyLoad function will be triggered only on scroll)
-      loader.src = 'https://www.eliananunes.com/images/lazy_loader.gif';
-      loader.addEventListener('load', checkImage);
-
-      // add the lazyLoad method onscroll
-      scrollableNode.onscroll = scrollableNode.addEventListener('scroll', lazyLoad);
-    }
-
-    return () => {
-      if (scrollableNode) {
-        // remove the lazyLoad method
-        scrollableNode.removeEventListener('scroll', lazyLoad);
+  const loadImage = (element) => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const { isIntersecting } = entry;
+          if (isIntersecting) {
+            setImgSrc(src);
+            observer.unobserve(element);
+          }
+        });
+      },
+      {
+        threshold: 0.5,
       }
-    };
-  }, [lazyLoad, loader, checkImage, scrollableNode]);
+    );
+    observer.observe(element);
+  };
 
   return (
     <img
       ref={imageRef}
       style={{
-        maxWidth: imgSrc === 'https://www.eliananunes.com/images/lazy_loader.gif' ? 300 : '100%',
+        maxWidth: '100%',
       }}
       className={className}
       src={imgSrc}
